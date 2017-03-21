629a3ccf425bfe7562a7457b394edadd89a75e10
Sushi Bajracharya <Bajra.suzhil@gmail.com>
2017-03-21 11:04:46 +0545

    small edits

--- coord.c
+++ coord.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <conio.h>
-#include <windows.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<windows.h>
 
 COORD coord = {0,0};
 
